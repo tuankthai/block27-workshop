df081529b8ab9c85b9d631f057d244df3b9f1bc3
tuankthai <ttlive4jc@gmail.com>
2023-08-09 17:36:07 -0700

    finish workshop 27 - react form

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,24 +1,53 @@
 import { useState } from "react"
+const AUTHENTICATE_URL = "https://fsa-jwt-practice.herokuapp.com/authenticate"
 
 export default function Authenticate({ token }) {
     const [error, setError] = useState(null)
-  
+    const [successMessage, setSuccessMessage] = useState(null)
+    const [username, setUsername] = useState(null)
+
+    function handleClear() {
+
+        setError(null)
+        setSuccessMessage(null)
+        setUsername(null)
+    }
+
     async function handleClick() {
         console.log("handleClick")
         try {
-            
+            const response = await fetch(AUTHENTICATE_URL, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+
+            const result = await (response.json())
+            console.log(result)
+            setSuccessMessage(result.message)
+            setUsername(result.data.username)
+
+
         } catch (error) {
             setError(error.message)
         }
-        
+
     }
 
     return (
-        <>       
-            <h2>Authenticate!!!</h2>
+        <div className="cardClass">
+            <h2>Authenticate!</h2>
             {error && <p>{error}</p>}
+            {successMessage && <p>{successMessage}</p>}
+            {username && <p>username: {username}</p>}
+
+            <button onClick={handleClick}>Authenticate Token</button>
+            <br/>
             
-            <button onClick={handleClick }>Authenticate Token</button>
-        </>
+            <button onClick={handleClear}>Clear Token</button>
+        </div>
     )
 }
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
-const SIGNUP_URL ="https://fsa-jwt-practice.herokuapp.com/signup"
+const SIGNUP_URL = "https://fsa-jwt-practice.herokuapp.com/signup"
 
-export default function SignUpForm({setToken}) {
+export default function SignUpForm({ setToken }) {
     const [username, setUserName] = useState("")
     const [password, setPassword] = useState("")
     const [error, setError] = useState(null)
@@ -9,6 +9,12 @@ export default function SignUpForm({setToken}) {
     async function handleSubmit(e) {
         e.preventDefault();
         console.log("hello")
+        if (username.length !== 8) {
+
+            alert("username must have length of eight characters")
+            return
+        }
+
         try {
             const response = await fetch(SIGNUP_URL, {
                 method: 'POST',
@@ -18,38 +24,42 @@ export default function SignUpForm({setToken}) {
                 body: JSON.stringify({ username, password }),
             });
 
-            
+
             const result = await (response.json())
             console.log(result)
             setToken(result.token)
-            
+
         } catch (error) {
             setError(error.message)
-            
+
         }
-        
+
     }
 
     return (
-        <>
-            <h2>Sign Up !!!</h2>
-            { error && <p>{ error}</p>  }
+
+        <div className="cardClass">
+            <h2>Sign Up !</h2>
+            {error && <p>{error}</p>}
 
             <form onSubmit={handleSubmit}>
                 <label>
                     Username:
-                    <input value={ username} onChange={(e)=>{setUserName(e.target.value) }}/>
-                </label>
-                <br/>
-                <label>
-                    Password:
-                    <input value={password} onChange={(e)=>{setPassword(e.target.value) }}/>
+                    <input value={username} onChange={(e) => { setUserName(e.target.value) }} />
+                    {/* <input value={ username} onChange={handleChange}/> */}
                 </label>
                 <br />
+                <br />
+                <label>
+                    Password:
+                    <input value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                </label>
+                <br/>
+                <br/>
                 <button >submit</button>
 
             </form>
-        </>
+        </div>
     )
 
 }
